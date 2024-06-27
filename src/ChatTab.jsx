7705f6e95a01cfe81427d16801628b8c89693a8e
Blurri10 <mbasilnore.10@gmail.com>
2024-06-28 02:32:11 +0530

    Made code more readable

--- src/ChatTab.jsx
+++ src/ChatTab.jsx
@@ -3,24 +3,14 @@ import Card from "./Card.jsx";
 
 export default function ChatTab({num1}) {
 
-    if (num1 === 0) {
-        return (
-            <div className="ChatTab">
-                <h1>Chats</h1>
-                <Card user={1} navigateTo="chat1"/>
-                <Card user={2} navigateTo="chat2"/>
-                <Card user={3} navigateTo="chat3"/>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div className="ChatTab hide">
-                <h1>Chats</h1>
-                <Card user={1} navigateTo="chat1"/>
-                <Card user={2} navigateTo="chat2"/>
-                <Card user={3} navigateTo="chat3"/>
-            </div>
-        )
-    }
+    const classes = num1 === 0 ? "ChatTab" : "ChatTab hide"
+
+    return (
+        <div className={classes}>
+            <h1>Chats</h1>
+            <Card user={1} navigateTo="chat1"/>
+            <Card user={2} navigateTo="chat2"/>
+            <Card user={3} navigateTo="chat3"/>
+        </div>
+    )
 }
--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -3,19 +3,11 @@ import ChatPage from "./ChatPage.jsx";
 
 export default function ChatWindow({num2}) {
 
-    if (num2 === 0) {
-        return (
-            <div className="ChatWindow hide">
-                <ChatPage user={num2}/>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div className="ChatWindow">
-                <ChatPage user={num2}/>
-            </div>
-        )
-    }
+    const classes = num2 === 0 ? "ChatWindow hide" : "ChatWindow"
 
+    return (
+        <div className={classes}>
+            <ChatPage user={num2}/>
+        </div>
+    )
 }
